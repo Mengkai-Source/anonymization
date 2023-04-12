39c4e79c9a144284273c32d48e3a3133e57cc616
Mengkai_Xu <44271824+Mengkai-Source@users.noreply.github.com>
2023-04-12 15:46:14 -0400

    Update numberAnonymizers.py

--- anonymization/anonymizers/numberAnonymizers.py
+++ anonymization/anonymizers/numberAnonymizers.py
@@ -13,14 +13,15 @@ class NumberAnonymizer():
 
     def __init__(self, anonymization: Anonymization):
         self.anonymization = anonymization
-        self.number_regex= r"""(?x) # verbose regex
-        \b    # Start of word
-        (?=   # Look ahead to ensure that this word contains...
-        \w*  # (after any number of alphanumeric characters)
-        \d   # ...at least one digit.
-        )     # End of lookahead
-        \w+   # Match the alphanumeric word
-        \s*   # Match any following whitespace"""
+        #self.number_regex = r"""(?x) # verbose regex
+        #\b    # Start of word
+        #(?=   # Look ahead to ensure that this word contains...
+        #\w*  # (after any number of alphanumeric characters)
+        #\d   # ...at least one digit.
+        #)     # End of lookahead
+        #\w+   # Match the alphanumeric word
+        #\s*   # Match any following whitespace"""
+        self.number_regex = r"""[a-zA-Z]+-+\d+|[a-zA-Z]+\d+""" # Capture patterns: Letters+-+Digits, Letter+Digits
 
     def anonymize(self, text: str) -> str:
         return  return  self.anonymization.regex_anonymizer(text, self.number_regex, 'port_number') # 'random_number'
